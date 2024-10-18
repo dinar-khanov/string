025a817b5c6b269c973caf3bfb9ec2f6846e21f2
dinar-khanov <hanov.di@gmail.com>
2024-10-18 11:16:20 +0500

    Add files via upload

---
+++ string.py
@@ -0,0 +1,7 @@
+my_string=input("Введите ваш логин ") #оздайте переменную my_string и присвойте ей значение строки с произвольным текстом (функция input())
+print(len(my_string)) #Вывести количество символов введённого текста
+print(my_string.upper())#Выведите строку my_string в верхнем регистре
+print(my_string.upper().lower())#Выведите строку my_string в нижнем регистре.
+print(my_string.replace(" ",""))#Измените строку my_string, удалив все пробелы
+print(my_string[0])#Выведите первый символ строки my_string
+print(my_string[-1])#Выведите последний символ строки my_string
